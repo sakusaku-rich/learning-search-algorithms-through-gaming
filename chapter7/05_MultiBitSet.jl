9942905289e64167d0209d0b190c55d3f4d35551
sakusaku-rich <7summer.cube@gmail.com>
2023-04-11 00:38:15 +0000

    bugfix: multibitset

--- chapter7/05_MultiBitSet.jl
+++ chapter7/05_MultiBitSet.jl
@@ -3,88 +3,116 @@ include("03_ZobrishHash.jl")
 module MultiBitSet
 
 using ..Zobrish: Hash
-using ..WallMazeGame: Coord, WallMazeState, DX, DY, init_hash, advance!, legal_actions, is_done
+using ..WallMazeGame: Coord, WallMazeState, DX, DY, init_hash, advance!, legal_actions, is_done, to_string
 using Distributed
 using Dates: now, Millisecond
-using DataStructures: PriorityQueue, dequeue!, enqueue!
+using DataStructures: PriorityQueue, dequeue!, enqueue!, Deque
 
 struct Mat
-    bits::BitMatrix
-    function Mat(h::Int, w::Int)
-        new(falses(h, w))
+    bits::Vector{UInt64}
+    function Mat(h::Int64)
+        new([0 for _ in 1:h])
     end
-    function Mat(bits::BitMatrix)
-        new(bits)
+    function Mat(bits::Vector{UInt64})
+        new(copy(bits))
     end
 end
 
-Base.copy(mat::Mat) = Mat(copy(mat.bits))
+Base.copy(mat::Mat) = Mat(mat.bits)
 
 function get(mat::Mat, y::Int, x::Int)::Bool
-    mat.bits[y, x]
+    mat.bits[y] & (1 << (x-1)) != 0
 end
 
 function set!(mat::Mat, y::Int, x::Int)
-    mat.bits[y, x] = true
+    mat.bits[y] |= 2^(x-1)
 end
 
 function del!(mat::Mat, y::Int, x::Int)
-    mat.bits[y, x] = false
+    mat.bits[y] &= ~(1 << (x-1))
 end
 
 function up_mat(mat::Mat, h::Int)::Mat
     ret_mat = copy(mat)
     for y in 1:h-1
-        ret_mat.bits[y, :] .|= @view mat.bits[y+1, :]
+        ret_mat.bits[y] |= ret_mat.bits[y + 1]
     end
     ret_mat
 end
 
 function down_mat(mat::Mat, h::Int)::Mat
     ret_mat = copy(mat)
-    for y in 2:h
-        ret_mat.bits[y, :] .|= @view mat.bits[y-1, :]
+    for y in Iterators.countfrom(h, -1)
+        if y == 1
+            break
+        end
+        ret_mat.bits[y] |= ret_mat.bits[y - 1]
     end
     ret_mat
 end
 
-function left_mat(mat::Mat, w::Int)::Mat
+function left_mat(mat::Mat, h::Int64)::Mat
     ret_mat = copy(mat)
-    for x in 1:w-1
-        ret_mat.bits[:, x] .|= @view mat.bits[:, x+1]
+    for y in 1:h
+        ret_mat.bits[y] >>= 1
     end
     ret_mat
 end
 
-function right_mat(mat::Mat, w::Int)::Mat
+function right_mat(mat::Mat, h::Int64)::Mat
     ret_mat = copy(mat)
-    for x in 2:w
-        ret_mat.bits[:, x] .|= @view mat.bits[:, x-1]
+    for y in 1:h
+        ret_mat.bits[y] <<= 1
     end
     ret_mat
 end
 
-function expand!(mat::Mat, h::Int, w::Int)
+function print_multi_bit_set(bits::Vector{UInt64})
+    for bit in bits
+        println(
+            lpad(
+                lpad(string(bit, base=2), 7, "0"),
+                50,
+                " "
+            )
+        )
+    end
+    println()
+end
+
+function expand!(mat::Mat, h::Int)
     up = up_mat(mat, h)
     down = down_mat(mat, h)
-    left = left_mat(mat, w)
-    right = right_mat(mat, w)
+    left = left_mat(mat, h)
+    right = right_mat(mat, h)
     mat.bits .|= up.bits
     mat.bits .|= down.bits
     mat.bits .|= left.bits
     mat.bits .|= right.bits
 end
 
-function andeq_not!(mat1::Mat, mat2::Mat)
-    mat1.bits .&= .!mat2.bits
+function andeq_not!(mat1::Mat, mat2::Mat, h::Int64)
+    for y in 1:h
+        mat1.bits[y] &= ~mat2.bits[y]
+    end
 end
 
-function is_equal(mat1::Mat, mat2::Mat)::Bool
-    mat1.bits == mat2.bits
+function is_equal(mat1::Mat, mat2::Mat, h::Int64)::Bool
+    for y in 1:h
+        if mat1.bits[y] != mat2.bits[y]
+            return false
+        end
+    end
+    true
 end
 
-function is_any_equal(mat1::Mat, mat2::Mat)::Bool
-    any(mat1.bits .& mat2.bits)
+function is_any_equal(mat1::Mat, mat2::Mat, h::Int64)::Bool
+    for y in 1:h
+        if mat1.bits[y] & mat2.bits[y] != 0
+            return true
+        end
+    end
+    false
 end
 
 mutable struct MazeStateByBitSet
@@ -92,9 +120,9 @@ mutable struct MazeStateByBitSet
     walls::Mat
     base_state::WallMazeState
 
-    function MazeStateByBitSet(state::WallMazeState)
-        whole_point_mat = Mat(state.h, state.w)
-        walls = Mat(state.h, state.w)
+    function MazeStateByBitSet(state::WallMazeState, h::Int)
+        whole_point_mat = Mat(h)
+        walls = Mat(h)
         for y in 1:state.h
             for x in 1:state.w
                 if state.walls[y, x] == 1
@@ -109,18 +137,19 @@ mutable struct MazeStateByBitSet
     end
 end
 
-# 0.000001 seconds (4 allocations: 400 bytes)
 function get_distance_to_nearest_point(state::MazeStateByBitSet)::Int
-    now_mat = Mat(state.base_state.h, state.base_state.w)
+    now_mat = Mat(state.base_state.h)
     set!(now_mat, state.base_state.character.y, state.base_state.character.x)
+    mask::UInt64 = 0x7f
     for depth in Iterators.countfrom(1)
-        if is_any_equal(now_mat, state.whole_point_mat)
+        if is_any_equal(now_mat, state.whole_point_mat, state.base_state.h)
             return depth
         end
         next_mat = copy(now_mat)
-        expand!(next_mat, state.base_state.h, state.base_state.w)
-        andeq_not!(next_mat, state.walls)
-        if is_equal(now_mat, next_mat)
+        expand!(next_mat, state.base_state.h)
+        next_mat.bits .&= mask
+        andeq_not!(next_mat, state.walls, state.base_state.h)
+        if is_equal(next_mat, now_mat, state.base_state.h)
             break
         end
         now_mat = next_mat
@@ -129,10 +158,12 @@ function get_distance_to_nearest_point(state::MazeStateByBitSet)::Int
 end
 
 function evaluate!(state::WallMazeState, h::Int, w::Int)
-    state.evaluated_score = state.game_score * h * w - get_distance_to_nearest_point(MazeStateByBitSet(state))
+    distance = get_distance_to_nearest_point(MazeStateByBitSet(state, h))
+    state.evaluated_score = state.game_score * h * w - distance
 end
 
 function beam_search_action(state::WallMazeState, beam_width::Int, beam_depth::Int)::Int64
+
     now_beam = PriorityQueue{WallMazeState, Int64}(Base.Order.Reverse)
     push!(now_beam, state => state.evaluated_score)
     best_state = state
@@ -160,9 +191,10 @@ function beam_search_action(state::WallMazeState, beam_width::Int, beam_depth::I
             end
         end
         now_beam = next_beam
-        if !isempty(now_beam)
-            best_state = first(now_beam)[1]
+        if isempty(now_beam)
+            break
         end
+        best_state = first(now_beam)[1]
         if is_done(best_state)
             break
         end
